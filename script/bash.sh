b00d8f6bc067d661e9ac6646f1e4f2d755e1536a
dizzy <1310135753@qq.com>
2024-11-26 14:52:32 +0800

    train

---
+++ script/bash.sh
@@ -0,0 +1,2 @@
+python train_one.py --source_path id2_ori --model_path ./output/id2 --use_ict True
+python test_one.py --source_path id2_ori --model_path ./output/id2 --use_ict True --render_train True --load_iteration 20000
