fe5e42ad41da8123e0e8008d95494f55fc5b40e5
dizzy <1310135753@qq.com>
2024-11-28 10:33:11 +0800

    test_sup

--- script/bash.sh
+++ script/bash.sh
@@ -1,2 +1,7 @@
 python train_one.py --source_path id2_ori --model_path ./output/id2 --use_ict True
 python test_one.py --source_path id2_ori --model_path ./output/id2 --use_ict True --render_train True --load_iteration 20000
+
+python script/convert_splat.py --input_dir ./output/id2_ori/ply_train
+
+quicksrnet 
+FaceUnet
